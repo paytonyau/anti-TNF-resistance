2b6e3faf9a64c3dbef3b9aae2c250873d83b6a89
Payton Tung On Yau <32238078+paytonyau@users.noreply.github.com>
2021-08-11 20:16:13 +0100

    Update 03A.MCP-Counter.R

--- 03A.MCP-Counter.R
+++ 03A.MCP-Counter.R
@@ -1,6 +1,9 @@
 #### xCELL and CIBERSORT were using the web tools
 
-###MCP-Counter
+#### analysis on Deconvolution-To-Estimate-Immune-Cells followed by the authors' github insruction
+#### https://github.com/holiday01/deconvolution-to-estimate-immune-cell-subsets
+
+##### MCP-Counter #####
 ## https://github.com/ebecht/MCPcounter
 ## Install  R devtools and MCPcounter dependancy 'curl'
 install.packages(c("devtools","curl")) 
@@ -21,11 +24,11 @@ expression = read.csv("merged_batch_effect_corrected_matrix", header = TRUE, row
 # )
 #### end
 
-# Run the program
+## Run the program
 Estimates = MCPcounter.estimate(expression, featuresType = "HUGO_symbols")
 write.csv(Estimates, "MCPcounter_matrix.csv")
 
-###### EPIC
+##### EPIC #####
 ### https://github.com/GfellerLab/EPIC
 devtools::install_github("GfellerLab/EPIC", build_vignettes=TRUE)
 ## Install the package
